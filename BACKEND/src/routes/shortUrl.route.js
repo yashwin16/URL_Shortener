c2fb630bff1ec1d76bbc0d886052712e5a638918
Yashwin Sharma <yashwinsharma16@gmail.com>
2025-07-17 15:53:23 +0530

    some changes

--- BACKEND/src/routes/shortUrl.route.js
+++ BACKEND/src/routes/shortUrl.route.js
@@ -1,7 +1,8 @@
 import express from 'express';
 import { createShortUrl } from '../controller/shortUrl.controller.js';
+import { authMiddleware } from '../middleware/auth.middleware.js';
 const router = express.Router();
 
-router.post("/",createShortUrl);
+router.post("/",authMiddleware,createShortUrl);
 
 export default router;
