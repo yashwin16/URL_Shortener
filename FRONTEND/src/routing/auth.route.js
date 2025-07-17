202a65c04ee03aac6b618d3c8eed8961907b3841
Yashwin Sharma <yashwinsharma16@gmail.com>
2025-07-17 13:32:52 +0530

    main.jsx or dashboard.js

--- FRONTEND/src/routing/auth.route.js
+++ FRONTEND/src/routing/auth.route.js
@@ -1,6 +1,6 @@
 import { createRoute } from "@tanstack/react-router"
 import AuthPage from "../pages/AuthPage"
-import { rootRoute } from "./routeTree"
+import { rootRoute } from "./Routetree.js"
 
 export const authRoute = createRoute({
     getParentRoute: () => rootRoute,
