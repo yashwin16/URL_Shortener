2f5e0ad6d01e445479eff2d0a4db5ec657070b4e
Yashwin Sharma <yashwinsharma16@gmail.com>
2025-07-17 15:21:19 +0530

    package minor change

--- BACKEND/src/config/config.js
+++ BACKEND/src/config/config.js
@@ -1,6 +1,6 @@
 export const cookieOptions = {
-    httpOnly: true,
-    secure: process.env.NODE_ENV === 'production', // Use secure cookies in production
-    maxAge: 24 * 60 * 60 * 1000,
-    sameSite: 'lax',
+  httpOnly: true,
+  secure: true, // Must be true for cross-site cookie on HTTPS
+  maxAge: 24 * 60 * 60 * 1000,
+  sameSite: 'None', // Must be 'None' for cross-site cookie to work
 };
